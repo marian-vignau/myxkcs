a09bbad92b3ec6a984e48ac69b6302224e9babfc
mavignau@gmail.com <mavignau@gmail.com>
2019-06-22 16:31:38 -0300

    show comic in panel

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
+import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
+
+import {
+  Widget
+} from '@phosphor/widgets';
 
 import '../style/index.css';
 
@@ -9,11 +17,49 @@ import '../style/index.css';
  * Initialization data for the myxkcd extension.
  */
 const extension: JupyterLabPlugin<void> = {
-  id: 'myxkcd',
-  autoStart: true,
-  activate: (app: JupyterLab) => {
-    console.log('JupyterLab extension myxkcd is activated!');
-  }
+    id: 'myxkcd',
+    autoStart: true,
+    requires: [ICommandPalette],
+    activate: (app: JupyterLab, palette: ICommandPalette) => {
+        console.log('JupyterLab extension myxkcd widget is activated!');
+
+        // Create a single widget
+        let widget: Widget = new Widget();
+        widget.id = 'xkcd-jupyterlab';
+        widget.title.label = 'xkcd.com';
+        widget.title.closable = true;
+
+        // Add an image element to the panel
+        let img = document.createElement('img');
+        widget.node.appendChild(img);
+
+        // Fetch info about a random comic
+        fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
+          return response.json();
+        }).then(data => {
+          img.src = data.img;
+          img.alt = data.title;
+          img.title = data.alt;
+        });
+        console.log('see the widget');
+
+        // Add an application command
+        const command: string = 'xkcd:open';
+        app.commands.addCommand(command, {
+        label: 'Random xkcd comic',
+        execute: () => {
+          if (!widget.isAttached) {
+            // Attach the widget to the main work area if it's not there
+            app.shell.addToMainArea(widget);
+          }
+          // Activate the widget
+          app.shell.activateById(widget.id);
+        }
+        });
+
+        // Add the command to the palette.
+        palette.addItem({command, category: 'Tutorial'});
+    }
 };
 
 export default extension;
